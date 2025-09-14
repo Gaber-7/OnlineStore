fda259dfdbdb1013cc7d06a00c71e39c4dca15f0
Gaber-7 <gaberalkholy053@gmail.com>
2025-09-14 09:33:04 +0100

    Initial commit - add eCommerce project

---
+++ Ecom.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Ecom.Core.Entites.Product.Product> Products { get; set; }
+        public virtual DbSet<Ecom.Core.Entites.Product.Category> Categories { get; set; }
+        public virtual DbSet<Ecom.Core.Entites.Product.Photo> Photos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
